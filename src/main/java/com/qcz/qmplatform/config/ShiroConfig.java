fc843e725428013d21f69817fc69b763f11c67b7
qcz_left <qcz_left@163.com>
2020-12-31 13:59:39 +0800

    shiro 使用ehcache缓存

--- src/main/java/com/qcz/qmplatform/config/ShiroConfig.java
+++ src/main/java/com/qcz/qmplatform/config/ShiroConfig.java
@@ -4,13 +4,13 @@ import com.qcz.qmplatform.common.constant.Constant;
 import com.qcz.qmplatform.filter.LoginFilter;
 import com.qcz.qmplatform.module.system.realm.UserRealm;
 import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.cache.ehcache.EhCacheManager;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.session.mgt.ExecutorServiceSessionValidationScheduler;
 import org.apache.shiro.session.mgt.SessionManager;
 import org.apache.shiro.session.mgt.SessionValidationScheduler;
 import org.apache.shiro.session.mgt.eis.JavaUuidSessionIdGenerator;
 import org.apache.shiro.session.mgt.eis.SessionIdGenerator;
-import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
@@ -19,7 +19,6 @@ import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 
 import javax.servlet.Filter;
 import java.util.HashMap;
@@ -30,6 +29,12 @@ import java.util.Map;
 public class ShiroConfig {
 
     @Bean
+    public EhCacheManager ehCacheManager() {
+        System.setProperty("net.sf.ehcache.skipUpdateCheck", "true");
+        return new EhCacheManager();
+    }
+
+    @Bean
     public SessionManager sessionManager() {
         DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
         sessionManager.setSessionIdCookieEnabled(true);
@@ -37,6 +42,7 @@ public class ShiroConfig {
         sessionManager.setSessionValidationScheduler(sessionValidationScheduler());
         sessionManager.setSessionValidationSchedulerEnabled(true);
         sessionManager.setSessionIdCookie(simpleCookie());
+        sessionManager.setCacheManager(ehCacheManager());
         return sessionManager;
     }
 
@@ -69,6 +75,7 @@ public class ShiroConfig {
         // 设置认证密码算法及迭代复杂度
         userRealm.setCredentialsMatcher(credentialsMatcher);
         userRealm.setCachingEnabled(true);
+        userRealm.setCacheManager(ehCacheManager());
         // 认证
         userRealm.setAuthenticationCachingEnabled(false);
         // 授权
