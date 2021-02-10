cb7583bcae763d19ca22f23135328f79adfd26a6
qcz_left <qcz_left@163.com>
2021-02-10 21:01:36 +0800

    添加注释

--- src/main/java/com/qcz/qmplatform/module/sche/DeleteTmpFileSchedule.java
+++ src/main/java/com/qcz/qmplatform/module/sche/DeleteTmpFileSchedule.java
@@ -2,14 +2,20 @@ package com.qcz.qmplatform.module.sche;
 
 import com.qcz.qmplatform.common.utils.ConfigLoader;
 import com.qcz.qmplatform.common.utils.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 定时删除临时文件
  */
 public class DeleteTmpFileSchedule {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DeleteTmpFileSchedule.class);
+
     public void run() {
-        FileUtils.clean(ConfigLoader.getDeleteTmpPath());
+        String deleteTmpPath = ConfigLoader.getDeleteTmpPath();
+        LOGGER.debug("Delete temporary file: {}, length: {}", deleteTmpPath, FileUtils.file(deleteTmpPath).length());
+        FileUtils.clean(deleteTmpPath);
     }
 
 }
