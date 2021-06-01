aad6be9572e6df31b0f269932f1b54385d402365
qcz_left <qcz_left@163.com>
2021-06-01 15:54:51 +0800

    调整代码生成器，调整xml文件位置、不生成Service接口

--- src/main/java/com/qcz/qmplatform/generator/GenerateCode.java
+++ src/main/java/com/qcz/qmplatform/generator/GenerateCode.java
@@ -1,15 +1,23 @@
 package com.qcz.qmplatform.generator;
 
 import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.FileOutConfig;
 import com.baomidou.mybatisplus.generator.config.GlobalConfig;
 import com.baomidou.mybatisplus.generator.config.PackageConfig;
 import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 import com.qcz.qmplatform.common.utils.StringUtils;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class GenerateCode {
@@ -44,6 +52,7 @@ public class GenerateCode {
         gc.setOutputDir(projectPath + "/src/main/java");
         gc.setAuthor("quchangzhong");
         gc.setOpen(false);
+        gc.setServiceImplName("%sService");
         mpg.setGlobalConfig(gc);
 
         // 数据源配置
@@ -59,21 +68,49 @@ public class GenerateCode {
         pc.setModuleName(scanner("模块名"));
         pc.setParent("com.qcz.qmplatform.module");
         pc.setEntity("domain");
+        Map<String, String> pathInfo = new HashMap<>();
+        String parent = projectPath + "/src/main/java/com/qcz/qmplatform/module/" + pc.getModuleName() + "/";
+        pathInfo.put("xml_path", projectPath + "/src/main/resources/mapper/" + pc.getModuleName());
+        pathInfo.put("entity_path", parent + pc.getEntity());
+        pathInfo.put("mapper_path", parent + pc.getMapper());
+        pathInfo.put("controller_path", parent + pc.getController());
+        pc.setPathInfo(pathInfo);
         mpg.setPackageInfo(pc);
 
         // 策略配置
         StrategyConfig strategy = new StrategyConfig();
         strategy.setNaming(NamingStrategy.underline_to_camel);
         strategy.setColumnNaming(NamingStrategy.underline_to_camel);
-//		strategy.setSuperEntityClass("com.baomidou.ant.common.BaseEntity");
         strategy.setEntityLombokModel(false);
         strategy.setRestControllerStyle(false);
-		strategy.setSuperControllerClass("com.qcz.qmplatform.module.base.BaseController");
+        strategy.setSuperControllerClass("com.qcz.qmplatform.module.base.BaseController");
         strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
         strategy.setControllerMappingHyphenStyle(true);
-        strategy.setTablePrefix("ope_");
+        strategy.setTablePrefix("sys_");
         strategy.setEntityTableFieldAnnotationEnable(true);
         mpg.setStrategy(strategy);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+
+        String templatePath = "/ftl/service.java.ftl";
+        // 自定义输出配置
+        List<FileOutConfig> focList = new ArrayList<>();
+        // 自定义配置会被优先输出
+        focList.add(new FileOutConfig(templatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名
+                return parent + "service/" + tableInfo.getEntityName() + "Service" + StringPool.DOT_JAVA;
+            }
+        });
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
 
         mpg.execute();
     }
---
+++ src/main/resources/ftl/service.java.ftl
@@ -0,0 +1,25 @@
+package ${package.Service};
+
+import ${package.Entity}.${entity};
+import ${package.Mapper}.${table.mapperName};
+import ${superServiceImplClassPackage};
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *${table.comment!} 服务实现类
+ * </p>
+ *
+ * @author ${author}
+ * @since ${date}
+ */
+@Service
+<#if kotlin>
+open class ${table.serviceImplName} : ${superServiceImplClass}<${table.mapperName}, ${entity}>(), ${table.serviceName} {
+
+}
+<#else>
+public class ${table.serviceImplName} extends ${superServiceImplClass}<${table.mapperName}, ${entity}> {
+
+}
+</#if>
