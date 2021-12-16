f1f55bdb47ed3780f3dc2189e4ff6b9c05babdf4
qcz_left <qcz_left@163.com>
2021-12-16 19:25:25 +0800

    程序启动时先初始化一下（第一次在操作日志里面使用IdUtils会很慢，暂查不到原因）

--- src/main/java/com/qcz/qmplatform/common/utils/IdUtils.java
+++ src/main/java/com/qcz/qmplatform/common/utils/IdUtils.java
@@ -1,11 +1,22 @@
 package com.qcz.qmplatform.common.utils;
 
 import cn.hutool.core.util.IdUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
 
-public class IdUtils extends IdUtil {
+@Component
+public class IdUtils extends IdUtil implements InitializingBean {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(IdUtils.class);
 
     private IdUtils() {
 
     }
 
+    @Override
+    public void afterPropertiesSet() {
+        LOGGER.debug("Initializing IdUtils: {}", simpleUUID());
+    }
 }
