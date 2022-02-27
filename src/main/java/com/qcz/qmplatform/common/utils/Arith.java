1383fd646aa5f825354522949596444b6eccb304
qcz_left <qcz_left@163.com>
2022-02-27 10:37:05 +0800

    修复：https://github.com/qcz-left/springboot-layui-qmplatform/issues/9

--- src/main/java/com/qcz/qmplatform/common/utils/Arith.java
+++ src/main/java/com/qcz/qmplatform/common/utils/Arith.java
@@ -79,6 +79,9 @@ public class Arith {
         if (scale < 0) {
             throw new IllegalArgumentException("The scale must be a positive integer or zero");
         }
+        if (v2 == 0) {
+            return 0;
+        }
         BigDecimal b1 = new BigDecimal(Double.toString(v1));
         BigDecimal b2 = new BigDecimal(Double.toString(v2));
         return b1.divide(b2, scale, BigDecimal.ROUND_HALF_UP).doubleValue();
