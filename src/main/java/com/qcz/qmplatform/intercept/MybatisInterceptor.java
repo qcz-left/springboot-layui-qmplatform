e972414750fa21e7efa2ba65b9514797c0fb74d4
qcz <qcz_left@163.com>
2023-08-30 11:00:28 +0800

    Mybatis拦截器自定义简写动态sql增加 $paramName$ 参数方式，对应 Mybatis 的 $，直接替换字符

--- src/main/java/com/qcz/qmplatform/intercept/MybatisInterceptor.java
+++ src/main/java/com/qcz/qmplatform/intercept/MybatisInterceptor.java
@@ -6,6 +6,7 @@ import cn.hutool.core.util.ReflectUtil;
 import com.qcz.qmplatform.common.constant.Constant;
 import com.qcz.qmplatform.common.utils.StringUtils;
 import com.qcz.qmplatform.common.utils.SubjectUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.cache.CacheKey;
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.BoundSql;
@@ -54,6 +55,7 @@ import java.util.regex.Pattern;
         method = "update",
         args = {MappedStatement.class, Object.class}
 )})
+@Slf4j
 public class MybatisInterceptor implements Interceptor {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MybatisInterceptor.class);
@@ -77,7 +79,8 @@ public class MybatisInterceptor implements Interceptor {
         final Class<?> clazz = Class.forName(className);
 
         Object parameter = args[PARAM_OBJ_INDEX];
-        BoundSql boundSql = ms.getBoundSql(parameter);;
+        BoundSql boundSql = ms.getBoundSql(parameter);
+        ;
         CacheKey cacheKey = null;
         Executor executor = (Executor) invocation.getTarget();
         RowBounds rowBounds = null;
@@ -191,20 +194,29 @@ public class MybatisInterceptor implements Interceptor {
         Matcher matcher = ENC_PATTERN.matcher(sql);
         while (matcher.find()) {
             String group = matcher.group();
-            String name = group.substring(group.indexOf("#") + 1, group.lastIndexOf("#"));
+
+            int indexOf = group.indexOf("#");
+            boolean isPlaceHolder = indexOf > -1;
+            indexOf = isPlaceHolder ? indexOf : group.indexOf("$");
+            int lastIndexOf = group.lastIndexOf("#");
+            lastIndexOf = isPlaceHolder ? lastIndexOf : group.lastIndexOf("$");
+
+            String name = group.substring(indexOf + 1, lastIndexOf);
             Object value = parameterMap.get(name);
             if (ObjectUtil.isNotEmpty(value)) {
-                sql = sql.replaceFirst(DYNAMIC_SQL_REG, group.substring(2, group.length() - 2));
-                sql = setParameterAndMapping(sql, name, value, newParameterMappings, configuration, additionalParameter);
+                sql = sql.replaceFirst(DYNAMIC_SQL_REG, Matcher.quoteReplacement(group.substring(2, group.length() - 2)));
+                sql = setParameter(sql, name, value, newParameterMappings, configuration, additionalParameter, isPlaceHolder);
             } else {
                 sql = sql.replaceFirst(DYNAMIC_SQL_REG, "");
             }
         }
 
         for (String key : parameterMap.keySet()) {
-            if (Pattern.compile("#" + key + "#").matcher(sql).find()) {
+            boolean isPlaceHolder = Pattern.compile("#" + key + "#").matcher(sql).find();
+            boolean isReplace = Pattern.compile(Matcher.quoteReplacement("$" + key + "$")).matcher(sql).find();
+            if (isPlaceHolder || isReplace) {
                 Object value = parameterMap.get(key);
-                sql = setParameterAndMapping(sql, key, value, newParameterMappings, configuration, additionalParameter);
+                sql = setParameter(sql, key, value, newParameterMappings, configuration, additionalParameter, isPlaceHolder);
             }
         }
 
@@ -220,8 +232,53 @@ public class MybatisInterceptor implements Interceptor {
         return newBoundSql;
     }
 
+    private String setParameter(String sql, String key, Object value, List<ParameterMapping> newParameterMappings, Configuration configuration, Map<String, Object> additionalParameter, boolean isPlaceHolder) {
+        if (isPlaceHolder) {
+            return setParameterAndMapping(sql, key, value, newParameterMappings, configuration, additionalParameter);
+        }
+
+        return setNormalParameter(sql, key, value);
+    }
+
+    /**
+     * 直接替换的方式设置普通参数
+     *
+     * @return sql
+     */
+    private String setNormalParameter(String sql, String key, Object value) {
+        StringBuilder replacement = new StringBuilder();
+        String replacementStr = "";
+        if (value instanceof List) {
+            List<?> valList = (List<?>) value;
+            for (Object o : valList) {
+                replacement.append(",'").append(o).append("'");
+            }
+            replacementStr = replacement.deleteCharAt(0).toString();
+        } else if (value instanceof Object[]) {
+            Object[] valArray = (Object[]) value;
+            for (Object o : valArray) {
+                replacement.append(",'").append(o).append("'");
+            }
+            replacementStr = replacement.deleteCharAt(0).toString();
+        } else if (value instanceof String) {
+            replacementStr = "'" + value + "'";
+        } else if (value instanceof Integer) {
+            replacementStr = "" + value + "";
+        } else {
+            log.warn("the parameter type[{}] is not supported!", value.getClass());
+            return sql;
+        }
+        return sql.replaceFirst(Matcher.quoteReplacement("$" + key + "$"), replacementStr);
+    }
+
+    /**
+     * 设置带有占位符的参数
+     *
+     * @return sql
+     */
     private String setParameterAndMapping(String sql, String key, Object value, List<ParameterMapping> newParameterMappings, Configuration configuration, Map<String, Object> additionalParameter) {
         StringBuilder replacement = new StringBuilder();
+        String replacementStr;
         if (value instanceof List) {
             List<?> valList = (List<?>) value;
             for (int i = 0; i < valList.size(); i++) {
@@ -231,7 +288,7 @@ public class MybatisInterceptor implements Interceptor {
                 newParameterMappings.add(parameterMapping);
                 additionalParameter.put("__frch_" + key + "_" + i, o);
             }
-            sql = sql.replaceFirst("#" + key + "#", replacement.deleteCharAt(0).toString());
+            replacementStr = replacement.deleteCharAt(0).toString();
         } else if (value instanceof Object[]) {
             Object[] valArray = (Object[]) value;
             for (int i = 0; i < valArray.length; i++) {
@@ -241,15 +298,14 @@ public class MybatisInterceptor implements Interceptor {
                 newParameterMappings.add(parameterMapping);
                 additionalParameter.put("__frch_" + key + "_" + i, o);
             }
-            sql = sql.replaceFirst("#" + key + "#", replacement.deleteCharAt(0).toString());
+            replacementStr = replacement.deleteCharAt(0).toString();
         } else {
-            // String
-            sql = sql.replaceFirst("#" + key + "#", "?");
+            replacementStr = "?";
             ParameterMapping parameterMapping = new ParameterMapping.Builder(configuration, key, value.getClass()).build();
             newParameterMappings.add(parameterMapping);
             additionalParameter.put(key, value);
         }
-        return sql;
+        return sql.replaceFirst("#" + key + "#", replacementStr);
     }
 
 }
