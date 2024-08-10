2bb0c495c153d1a035fae1edb51bef7c7ab9b097
qcz_left <qcz_left@163.com>
2024-08-10 11:54:33 +0800

    JSON工具类添加方法：将对象转为格式化后的字符串

--- src/main/java/com/qcz/qmplatform/common/utils/JSONUtils.java
+++ src/main/java/com/qcz/qmplatform/common/utils/JSONUtils.java
@@ -7,4 +7,14 @@ import cn.hutool.json.JSONUtil;
  */
 public class JSONUtils extends JSONUtil {
 
+    /**
+     * 将对象转为格式化后的JSON字符串
+     *
+     * @param jsonObject 对象
+     * @return 格式化后的JSON字符串
+     */
+    public static String formatJson(Object jsonObject) {
+        return formatJsonStr(toJsonStr(jsonObject));
+    }
+
 }
