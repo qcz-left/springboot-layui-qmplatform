0cc8e486d22d2f167e953b9c942d396c9f1a96bd
qcz <qcz_left@163.com>
2024-09-11 10:54:24 +0800

    线程池调整，初始化线程数 20，最大线程数 2048

--- src/main/java/com/qcz/qmplatform/common/utils/ThreadPoolUtils.java
+++ src/main/java/com/qcz/qmplatform/common/utils/ThreadPoolUtils.java
@@ -1,6 +1,6 @@
 package com.qcz.qmplatform.common.utils;
 
-import cn.hutool.core.thread.ThreadUtil;
+import cn.hutool.core.thread.ExecutorBuilder;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.Future;
@@ -14,13 +14,25 @@ import java.util.concurrent.ThreadPoolExecutor;
 @Slf4j
 public class ThreadPoolUtils {
 
-    private static final ThreadPoolExecutor EXECUTOR_SERVICE = ThreadUtil.newExecutor(10, 50);
+    /**
+     * 初始化线程数
+     */
+    private static final int CORE_POOL_SIZE = 20;
+    /**
+     * 最大线程数
+     */
+    private static final int MAX_POOL_SIZE = 2048;
+
+    private static final ThreadPoolExecutor EXECUTOR_SERVICE = ExecutorBuilder.create()
+            .setCorePoolSize(CORE_POOL_SIZE)
+            .setMaxPoolSize(MAX_POOL_SIZE)
+            .build();
 
     /**
      * 执行一条线程任务，无返回
      */
     public static void execute(Runnable runnable) {
-        log.info("add a task, ActiveCount: {}, MaximumPoolSize: {}", EXECUTOR_SERVICE.getActiveCount(), EXECUTOR_SERVICE.getMaximumPoolSize());
+        log.info("add a task, ActiveCount: {}, QueueSize: {}", EXECUTOR_SERVICE.getActiveCount(), EXECUTOR_SERVICE.getQueue().size());
         EXECUTOR_SERVICE.execute(runnable);
     }
 
