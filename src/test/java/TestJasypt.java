b717074eee0de4e7d384ba3e53cd0b0f0c437bd4
qcz <qcz_left@163.com>
2025-07-15 13:49:53 +0800

    jasypt 升级到3.0.5，单元测试包 设置scope test

--- src/test/java/TestJasypt.java
+++ src/test/java/TestJasypt.java
@@ -1,12 +1,10 @@
-package com.qcz.qmplatform;
-
+import com.qcz.qmplatform.QmplatformApplication;
+import jakarta.annotation.Resource;
 import org.jasypt.encryption.StringEncryptor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import jakarta.annotation.Resource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = QmplatformApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
